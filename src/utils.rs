794c670701d32b99c914a4acac755c3c48ef4c58
boxdot <d@zerovolt.org>
2017-05-02 00:13:38 +0200

    Extend chat protocol to link Spark and Gerrit accounts.
    * Move all communication messages from Spark to bot submodule.
    * Add xorshiftstar (Test needed).

---
+++ src/utils.rs
@@ -0,0 +1,19 @@
+pub fn transform_u64_to_array_of_u8(x: u64) -> [u8; 8] {
+    let b0: u8 = ((x >> 56) & 0xff) as u8;
+    let b1: u8 = ((x >> 48) & 0xff) as u8;
+    let b2: u8 = ((x >> 40) & 0xff) as u8;
+    let b3: u8 = ((x >> 32) & 0xff) as u8;
+    let b4: u8 = ((x >> 24) & 0xff) as u8;
+    let b5: u8 = ((x >> 16) & 0xff) as u8;
+    let b6: u8 = ((x >> 8) & 0xff) as u8;
+    let b7: u8 = (x & 0xff) as u8;
+    return [b0, b1, b2, b3, b4, b5, b6, b7];
+}
+
+pub fn xorshift64star(seed: u64) -> u64 {
+    let mut x = seed;
+    x ^= x >> 12;
+    x ^= x << 25;
+    x ^= x >> 27;
+    return x.wrapping_mul(0x2545F4914F6CDD1Du64);
+}
