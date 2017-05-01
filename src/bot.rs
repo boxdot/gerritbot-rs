fe283f94a292615c88c08e97bf18b88c30ae25f2
boxdot <d@zerovolt.org>
2017-05-01 11:24:57 +0200

    Implement actions.

--- src/bot.rs
+++ src/bot.rs
@@ -1,15 +1,17 @@
+use std::collections::HashSet;
+
 /// Spark id of the user
 pub type PersonId = String;
 
 /// Describes a state of the bot
 #[derive(Debug)]
 pub struct Bot {
-    pub persons: Vec<PersonId>,
+    pub persons: HashSet<PersonId>,
 }
 
 impl Bot {
     pub fn new() -> Bot {
-        return Bot { persons: Vec::new() };
+        return Bot { persons: HashSet::new() };
     }
 }
 
@@ -23,5 +25,15 @@ pub enum Action {
 
 /// Action controller
 pub fn update(action: Action, bot: Bot) -> Bot {
+    let mut bot = bot;
+    match action {
+        Action::Enable(person_id) => {
+            bot.persons.insert(person_id);
+        }
+        Action::Disable(person_id) => {
+            bot.persons.remove(&person_id);
+        }
+        _ => (),
+    }
     bot
 }
