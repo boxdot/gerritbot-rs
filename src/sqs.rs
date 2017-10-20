71572d141b9fceb7dd895d8edd5ca597f38799ed
boxdot <d@zerovolt.org>
2017-10-20 23:06:13 +0200

    Delete sqs messages async in the same core loop.

--- src/sqs.rs
+++ src/sqs.rs
@@ -1,7 +1,8 @@
 use std::{error, fmt, thread};
 
 use futures::{Sink, Future, Stream};
-use futures::sync::mpsc::{channel, Receiver, Sender};
+use futures::sync::mpsc::channel;
+use futures::stream::BoxStream;
 use rusoto_core::{self, default_tls_client, DefaultCredentialsProvider, Region};
 use rusoto_sqs::{self, Sqs, SqsClient, ReceiveMessageRequest, DeleteMessageRequest};
 
@@ -48,45 +49,14 @@ impl From<rusoto_core::TlsError> for Error {
     }
 }
 
-fn sqs_delete_worker(
-    queue_url: String,
-    queue_region: Region,
-) -> Result<Sender<String /* sqs receipt handle */>, Error> {
-    let aws_credentials = DefaultCredentialsProvider::new()?;
-    let sqs_client = SqsClient::new(default_tls_client()?, aws_credentials, queue_region);
-
-    let (del_tx, del_rx) = channel(1);
-    let del_th_queue_url = queue_url.clone();
-    thread::spawn(move || {
-        del_rx
-            .for_each(|receipt_handle| {
-                let delete_req = DeleteMessageRequest {
-                    queue_url: del_th_queue_url.clone(),
-                    receipt_handle: receipt_handle,
-                };
-                if let Err(e) = sqs_client.delete_message(&delete_req) {
-                    error!(
-                        "Could not delete message with handle {}: {:?}",
-                        delete_req.receipt_handle,
-                        e
-                    );
-                };
-                Ok(())
-            })
-            .wait()
-            .unwrap();
-    });
-    Ok(del_tx)
-}
-
 pub fn sqs_receiver(
     queue_url: String,
     queue_region: Region,
-) -> Result<Receiver<rusoto_sqs::Message>, Error> {
-    let del_tx = sqs_delete_worker(queue_url.clone(), queue_region)?;
-
+) -> Result<BoxStream<rusoto_sqs::Message, ()>, Error> {
+    // receive messages
     let aws_credentials = DefaultCredentialsProvider::new()?;
     let sqs_client = SqsClient::new(default_tls_client()?, aws_credentials, queue_region);
+
     let mut receive_req = ReceiveMessageRequest::default();
     receive_req.queue_url = queue_url.clone();
     receive_req.wait_time_seconds = Some(10);
@@ -103,17 +73,6 @@ pub fn sqs_receiver(
                             match tx_loop.clone().send(msg.clone()).wait() {
                                 Ok(s) => {
                                     tx_loop = s;
-                                    if let Some(receipt_handle) = msg.receipt_handle {
-                                        match del_tx.clone().send(receipt_handle).wait() {
-                                            Ok(_) => (),
-                                            Err(err) => {
-                                                error!(
-                                                    "Cannot send sqs msg delete task: {:?}",
-                                                    err
-                                                );
-                                            }
-                                        }
-                                    }
                                 }
                                 Err(err) => {
                                     error!("Cannot send message through channel: {:?}", err);
@@ -121,11 +80,32 @@ pub fn sqs_receiver(
                                 }
                             };
                         }
-                    }
+                    };
                 }
                 Err(err) => warn!("SQS client error: {:?}", err),
             }
         }
     });
-    Ok(rx)
+
+    // delete received messages
+    let aws_credentials = DefaultCredentialsProvider::new()?;
+    let sqs_client = SqsClient::new(default_tls_client()?, aws_credentials, queue_region);
+    let rx = rx.and_then(move |msg| {
+        if let Some(ref receipt_handle) = msg.receipt_handle {
+            let delete_req = DeleteMessageRequest {
+                queue_url: queue_url.clone(),
+                receipt_handle: receipt_handle.clone(),
+            };
+            if let Err(err) = sqs_client.delete_message(&delete_req) {
+                error!(
+                    "Could not delete message with handle {}: {:?}",
+                    delete_req.receipt_handle,
+                    err
+                )
+            };
+        }
+        Ok(msg)
+    });
+
+    Ok(rx.boxed())
 }
