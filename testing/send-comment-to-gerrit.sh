e3b11a0527a4f792123f7ec7a661802455ab8f90
Gabriel Féron <gabriel.feron@here.com>
2018-04-16 18:51:59 +0000

    Fix key path in send-comments script

--- testing/send-comment-to-gerrit.sh
+++ testing/send-comment-to-gerrit.sh
@@ -3,7 +3,7 @@
 # namely: a review by admin, with id=1 and jdoe user
 ssh -o UserKnownHostsFile=/dev/null \
     -o StrictHostKeyChecking=no \
-    -i ./id_rsa_jdoe \
+    -i ./data/id_rsa_jdoe \
     -p 29418 \
     jdoe@localhost \
     gerrit review ${1:-1,1} -m "$(shuf -n5 /usr/share/dict/american-english)"
