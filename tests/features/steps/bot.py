78cd8a1b79bd8f6705eb501106683c93493a99e9
Frank Benkstein <frank@benkstein.net>
2019-03-31 21:56:23 +0200

    more command tests

--- tests/features/steps/bot.py
+++ tests/features/steps/bot.py
@@ -1,18 +1,33 @@
+import itertools
+
 from hamcrest import *
 
 
-@when(u"we check for messages by the bot")
+@when("we check for messages by the bot")
 def step_impl(context):
     context.bot.get_messages()
 
 
-@then(u"there are no messages")
+@then("there are no messages")
 def step_impl(context):
     assert_that(context.bot.current_messages, empty())
 
 
-@then(u'there is a message for {person} which includes the text "{text}"')
+@then('there is a message for {person} which includes the text "{text}"')
 def step_impl(context, person, text):
+    context.execute_steps(
+        f'''
+    then there is a message for {person} which includes the following text:
+    """
+    {text}
+    """
+    '''
+    )
+
+
+@then("there is a message for {person} which includes the following text")
+def step_impl(context, person):
+    text = context.text
     person = context.persons.get(person)
     messages_for_person = context.bot.get_messages_for_person(person)
     item_matcher = has_entry("text", contains_string(text))
@@ -22,12 +37,16 @@ def step_impl(context, person, text):
     )
 
 
-@then(u'this message includes the text "{text}"')
+@then('this message includes the text "{text}"')
 def step_impl(context, text):
     assert_that(context.last_matched_message, has_entry("text", contains_string(text)))
 
 
-@given(u"{sender} sends the {command} command to the bot")
+@step("{sender} sends the {command} command to the bot")
 def step_impl(context, sender, command):
-    sender = context.persons.get(sender)
-    context.bot.send_message(sender, command)
+    if sender == "everybody":
+        for person in context.persons:
+            context.bot.send_message(person, command)
+    else:
+        sender = context.persons.get(sender)
+        context.bot.send_message(sender, command)
--- tests/features/steps/persons.py
+++ tests/features/steps/persons.py
@@ -8,3 +8,11 @@ class Person(object):
 def step_impl(context, name, email):
     person = context.persons.create(name, email)
     context.gerrit.create_user(person)
+
+
+@given("the following persons")
+def step_impl(context):
+    for row in context.table:
+        context.execute_steps(
+            f"given a person named {row['name']} with email address {row['email']}"
+        )
