b4ed1a9ee192b1fa3b6abd80a508f447fc8aa6a1
Frank Benkstein <frank@benkstein.net>
2019-04-09 22:55:40 +0200

    add test for inline comments
    These seem broken at the moment.

--- tests/features/formatting.feature
+++ tests/features/formatting.feature
@@ -36,3 +36,33 @@ Feature: message formatting
         """
         [{context.last_created_change[subject]}]({context.gerrit.http_url}/{context.last_created_change[_number]}) (bob) 👓 Added as reviewer
         """
+
+  # TODO: missing inline comments
+  @wipo
+  Scenario: inline comments
+     Given Bob uploads a new change to the tools project
+       And Bob creates the file "README" with the following content in the change:
+       """
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+       volutpat ornare convallis. Sed luctus imperdiet nisl, at malesuada risus
+       tincidunt vitae. Donec iaculis, lectus ac tempor ullamcorper, lorem odio
+       eleifend quam, id ullamcorper metus lacus quis lorem. Aenean fringilla,
+       erat vitae rhoncus ultrices, purus est pharetra erat, ac auctor nunc
+       sapien eget justo. Nam in iaculis lacus. Cras sit amet nibh libero. Etiam
+       non sem quis tortor efficitur ornare sit amet a eros. Aenean commodo
+       tempor lectus, id fermentum nisl semper nec. Mauris quis odio sit amet
+       nulla volutpat porta et et turpis.
+       """
+       And Alice replies to Bob's change with Code-Review-2 and the comment "Boo!" and the following inline comments:
+       """
+       File: README
+       Line 0: You shouldn't be adding this file in the first place.
+       Line 8: Who even is Mauris?
+       """
+      When we check for messages by the bot
+      Then there is a message for Bob with the following text:
+        """
+        [{context.last_created_change[subject]}]({context.gerrit.http_url}/{context.last_created_change[_number]}) (tools) 👎 -2 (Code-Review) from alice
+
+        > Boo!
+        """
--- tests/features/steps/gerrit.py
+++ tests/features/steps/gerrit.py
@@ -16,6 +16,16 @@ def step_impl(context, uploader, project_name):
     )
 
 
+@given(
+    '{uploader} creates the file "{filename}" with the following content in the change'
+)
+def step_impl(context, uploader, filename):
+    uploader = context.persons.get(uploader)
+    context.gerrit.add_file_to_change(
+        uploader, context.last_created_change, filename, context.text
+    )
+
+
 @given("{actor} adds {reviewer} as reviewer to {owner}'s change")
 def step_impl(context, actor, reviewer, owner):
     actor = context.persons.get(actor)
@@ -29,13 +39,46 @@ use_step_matcher("re")
 
 
 @given(
-    '(?P<reviewer>.*) replies to (?P<uploader>.*)\'s change with (?P<label_name>[^ ]*)(?P<label_value>[+-]\d+)(?: and the comment "(?P<comment>.*)")?'
+    "(?P<reviewer>.*) replies to (?P<uploader>.*)'s change with "
+    "(?P<label_name>[^ ]*)(?P<label_value>[+-]\d+)"
+    '(?: and the comment "(?P<comment>.*)")?'
+    "(?P<has_inline_comments> and the following inline comments)?"
 )
-def step_impl(context, reviewer, uploader, label_name, label_value, comment):
+def step_impl(
+    context, reviewer, uploader, label_name, label_value, comment, has_inline_comments
+):
     reviewer = context.persons.get(reviewer)
     uploader = context.persons.get(uploader)
     change = context.gerrit.get_last_change_by(uploader)
     label_value = int(label_value)
+
+    if has_inline_comments:
+        inline_comments = {}
+        filename = None
+        for m in re.finditer(
+            r"(^Line (?P<line>\d+): (?P<comment>.*)|File: (?P<filename>.*))$",
+            context.text,
+            re.MULTILINE,
+        ):
+            next_filename, line_number_str, line_comment = m.group(
+                "filename", "line", "comment"
+            )
+
+            if next_filename is not None:
+                filename = next_filename
+            else:
+                line_number = int(line_number_str)
+                inline_comments.setdefault(filename, []).append(
+                    {"line": line_number, "message": line_comment}
+                )
+
+    else:
+        inline_comments = None
+
     context.gerrit.reply(
-        change, reviewer, labels={label_name: label_value}, message=comment
+        change,
+        reviewer,
+        labels={label_name: label_value},
+        message=comment,
+        comments=inline_comments,
     )
