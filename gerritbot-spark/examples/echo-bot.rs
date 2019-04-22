9573dbb5ee5411c49f33f1a6dc93a0c1e815280a
Frank Benkstein <frank@benkstein.net>
2019-04-22 20:30:27 +0200

    change echo bot to reply by email instead of person_id

--- gerritbot-spark/examples/echo-bot.rs
+++ gerritbot-spark/examples/echo-bot.rs
@@ -70,8 +70,8 @@ fn main() {
                 let messages_future = messages.for_each(move |message| {
                     debug!("got a message: {:?}", message);
                     client
-                        .reply(
-                            &message.person_id,
+                        .send_message(
+                            &message.person_email,
                             &format!("got post:\n```\n{:#?}\n```", message),
                         )
                         .map_err(|e| error!("failed to send message: {}", e))
