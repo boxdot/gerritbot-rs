1826b4bb77d3e332dbddad330937e10a5708cc77
Frank Benkstein <frank@benkstein.net>
2019-05-13 18:36:59 +0200

    add command parse test

--- gerritbot/src/command.rs
+++ gerritbot/src/command.rs
@@ -40,3 +40,59 @@ impl FromStr for Command {
         })
     }
 }
+
+#[cfg(test)]
+mod test {
+    use assert_matches::assert_matches;
+
+    use super::Command;
+
+    macro_rules! test_parse {
+        ($name:ident, $s:expr, $( $c:tt )+) => {
+            #[test]
+            fn $name() {
+                assert_matches!($s.parse::<Command>().expect("parse failed"), $( $c )+);
+            }
+        };
+
+        ($name:ident, $c:pat) => {
+            test_parse!($name, stringify!($name), $c);
+        };
+    }
+
+    macro_rules! test_parse_fail {
+        ($name:ident, $s:expr) => {
+            #[test]
+            fn $name() {
+                assert_matches!($s.parse::<Command>().expect_err("parse didn't fail"), ());
+            }
+        };
+    }
+
+    test_parse!(enable, Command::Enable);
+    test_parse!(enable_with_whitespace, "\t\t   enable\n\n", Command::Enable);
+    test_parse!(enable_mixed_case, "EnAbLe", Command::Enable);
+    test_parse!(disable, Command::Disable);
+    test_parse!(status, Command::Status);
+    test_parse!(help, Command::Help);
+    test_parse!(version, Command::Version);
+    test_parse!(filter, Command::FilterStatus);
+    test_parse!(filter_enable, "filter enable", Command::FilterEnable(true));
+    test_parse!(
+        filter_disable,
+        "filter disable",
+        Command::FilterEnable(false)
+    );
+    test_parse!(
+        filter_set,
+        "filter abc def",
+        Command::FilterAdd(ref s) if s == "abc def"
+    );
+    test_parse!(
+        filter_set_with_whitespace,
+        "filter  abc def ",
+        Command::FilterAdd(ref s) if s == " abc def"
+    );
+
+    test_parse_fail!(unknown_command, "unknown");
+}
